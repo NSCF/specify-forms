b0e21e9dedaa6ef5d0378651bd1c381366129233
Ian Engelbrecht <ianicus.za@gmail.com>
2023-01-27 20:13:35 +0200

    added formatting to sortViews.py, to be tested

---
+++ sortViews.py
@@ -0,0 +1,96 @@
+#sorts the views and viewdefs in a views.xml file
+#moves the name attribute first
+#note this replaces the original file, so commit first...
+import re
+import os
+
+fileName = 'backstop/global.views.xml'
+
+def getElemName(xmlStr):
+  nameAttrStart = xmlStr.index('name')
+  #now we need the closing quote
+  nameStart = xmlStr.index('"', nameAttrStart) + 1
+  nameEnd = xmlStr.index('"', nameStart)
+  name = xmlStr[nameStart:nameEnd]
+  return name
+
+#takes a view or viewdef element and puts the name first, sets 'tabs', and removes white space before the end of the tag
+def formatFirstElement(elemStr):
+  endIndex = elemStr.index('>') #we want the first one, ie the opening tag and it's elements
+  tagAndAttrs = elemStr[0:endIndex] #this excludes the closing caret
+  theRest = elemStr[(endIndex + 2):len(elemStr)]
+  tagAndAttrsList = re.split('\s+', tagAndAttrs) #this effectively removes whitespace, but...
+  tagAndAttrsList = [x for x in tagAndAttrsList if x] #apparently we need to remove any empty strings...
+
+  #move the name attr to second place in list
+  if not tagAndAttrsList[1].startswith('name'): #this is what we want
+    nameAttrIndex = getNameAttrIndex(tagAndAttrsList)
+    nameAttr = tagAndAttrsList[nameAttrIndex]
+    del tagAndAttrsList[nameAttrIndex]
+    tagAndAttrsList.insert(1, nameAttr)
+
+  #add the necessary line breaks and tabs
+  tagAndAttrsList[0] = tagAndAttrsList[0] + ' ' #space after the tag
+  for ind in range(2, len(tagAndAttrsList)):
+    tagAndAttrsList[ind] = os.linesep + ('\t' * 6) + tagAndAttrsList[ind]
+
+  tagAndAttrsList.append('>') #add the closing caret for the tag again
+  cleanFirstTag = ''.join(tagAndAttrsList)
+  updatedElemStr = cleanFirstTag + theRest
+  
+  return updatedElemStr
+
+
+def getNameAttrIndex(elemAttrList):
+  for (index, item) in enumerate(elemAttrList):
+    if(item.startswith('name')):
+      return index
+
+
+def sortxmlElems(xmlstr, section):
+
+  openingtag = '<' + section + '>'
+  closingtag = '</' + section + '>'
+
+  xmlStart = xmlstr.index(openingtag)
+  xmlEnd = xmlstr.index(closingtag) + len(closingtag)
+  xmlString = xmlstr[xmlStart:xmlEnd]
+
+  #split into individual views
+  elements = []
+  searchStart = len(openingtag) + 1
+  elemOpeningTag = '<' + section[:-1]
+  elemClosingTag = '</' + section[:-1] + '>'
+  while searchStart < len(xmlString):
+    try:
+      elemStart = xmlString.index(elemOpeningTag, searchStart)
+    except:
+      break
+
+    elemEnd = xmlString.index(elemClosingTag, searchStart) + len(elemClosingTag)
+    elem = xmlString[elemStart:elemEnd]
+    elements.append(elem)
+
+    searchStart = elemEnd + 1
+  
+  elements.sort(key=getElemName)
+
+  #strings are immutable so we have to do this
+  firstxml = xmlstr[0:xmlStart]
+  lastxml = xmlstr[xmlEnd:]
+  newxml = firstxml + openingtag + '\n\n\t\t' + '\n\n\t\t'.join(elements) + '\n\n\t' + closingtag + lastxml
+
+  return newxml
+
+###the actual script ###
+
+with open(fileName, 'r') as f:
+  xml = f.read()
+
+sortedviewsxml = sortxmlElems(xml, 'views')
+sortedviewdefsxml = sortxmlElems(sortedviewsxml, 'viewdefs')
+
+with open(fileName, 'w') as f:
+  f.write(sortedviewdefsxml)
+
+print('all done')
